84d12389d2124bdc01ef467e9a76487eec7f4cb6
Alexey Aleynikov <alexey.aleynikov@telekom.com>
2024-05-15 19:03:39 +0300

    added test data

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
+    let b: u8;
     println!("Hello, world!");
+    // Boolean
+    let flag = true;
+    // Char
+    let ch = 'a';
+    //Compound
+    //Tuple
+    let my_typle: (i8, char, bool) = (42, 'a', false);
+    //Array
+    let arr = [1, 2, 3]; //fixed
 }
